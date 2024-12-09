e25575d962508a887831498aea8b155ed56a4ad5
leomenino <148128596+leomenino@users.noreply.github.com>
2024-12-09 20:21:11 +0000

    New project

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+function Home({ livros, setLivros }) {
+  const handleDelete = (id) => {
+    const novosLivros = livros.filter(livro => livro.id !== id);
+    setLivros(novosLivros);
+  };
+
+  return (
+    <div>
+      <h1>Lista de Livros</h1>
+      <Link to="/create">Adicionar Novo Livro</Link>
+      <ul>
+        {livros.map(livro => (
+          <li key={livro.id}>
+            <Link to={`/view/${livro.id}`}>
+              {livro.titulo} - {livro.autor} ({livro.ano})
+            </Link>
+            {" "}
+            <button onClick={() => handleDelete(livro.id)}>Excluir</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
