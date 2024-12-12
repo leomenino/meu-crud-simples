02889cb8ae356504b75db3af5ac153203e1b2770
leomenino <148128596+leomenino@users.noreply.github.com>
2024-12-12 22:04:25 +0000

    new book

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [livros, setLivros] = useState([
     { id: 1, titulo: "A voz do silêncio", autor: "Helena P. Blavatsky", ano: 1889 },
     { id: 2, titulo: "Os Iniciados do Fogo", autor: "Manly P. Hall", ano: 1922 },
-    { id: 3, titulo: "História secreta do mundo", autor: "Jonathan Black", ano:2007 }
+    { id: 3, titulo: "História Oculta do mundo", autor: "Édouard Schuré", ano: 1889 }
   ]);
 
   return (
